2689209b430106aba8f086cb891d222689de3ebd
prashant <prasanta.senapati@pramata.com>
2019-07-05 04:59:29 +0000

    initial commit

---
+++ app/controllers/food_controller.rb
@@ -0,0 +1,9 @@
+class FoodController < ApplicationController
+  def index
+  end
+
+  def login
+  	username = params[:user]
+  	password = params[:password]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'food/index'
+  get 'food/login'
+  root :to => "food#login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
