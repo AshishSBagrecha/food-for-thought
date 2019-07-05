5c5c668d877a0a8d2257a2e843846cb03d54546f
Ashish Bagrecha <ashish.bagrecha@pramata.com>
2019-07-05 05:36:54 +0000

    4 users created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([{first_name: 'Prashanth',last_name: 'Senapathi',location: 'Adugodi', email_id: 'prasanta.senapati@pramata.com ',user_type: 'Receiver',password: 'welcome@123'},{first_name: 'Ashish',last_name: 'Ahuja',location: 'BTM 1st Stage', email_id: 'ashish.ahuja@pramata.com',user_type: 'Receiver',password: 'welcome@123'},{first_name: 'Ashish',last_name: 'Bagrecha',location: 'Rajajinagar', email_id: 'ashish.bagrecha@pramata.com',user_type: 'Donor',password: 'welcome@123'},{first_name: 'Shubhendra',last_name: 'Pinge',location: 'Wilson Garden', email_id: 'shubhendra.pinge@pramata.com',user_type: 'Donor',password: 'welcome@123'}])
