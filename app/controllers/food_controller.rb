dc5d86562c1be25206a8f62ea41b5911845c43ca
prashant <prasanta.senapati@pramata.com>
2019-07-05 09:28:49 +0000

    commit

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -1,3 +1,4 @@
+require 'twilio-ruby'
 class FoodController < ApplicationController
 	skip_before_action :verify_authenticity_token
   def index
@@ -12,6 +13,7 @@ class FoodController < ApplicationController
   	if user.present? && user.password == password
        response[:status] = 200
        response[:message] = "success"
+       session[:user_id]= user.id
        response[:details] = {"first_name" => user.first_name,"last_name" => user.last_name,"user_type" => user.user_type} 
   	else
       response[:status] = 500
@@ -20,6 +22,17 @@ class FoodController < ApplicationController
   	render :json => response.to_json 
   end
 
+  def create_event
+  	generate_event(params)
+  	render :json => "success".to_json
+  end
+
+  def place_order
+  	p "==============place_order=================="
+  	create_order(params)
+  	render :json => "success".to_json
+  end
+ 
 	private
   	def generate_event_code
 		charset = Array('A'..'Z') + Array('a'..'z')
@@ -27,19 +40,18 @@ class FoodController < ApplicationController
 		return code
 	end
 
-	def create_event(params)
+	def generate_event(params)
 		location = params[:location]
-		available_meals = params[:available_meals]
+		available_meals = params[:hosted]
 		pno = params[:phone_number]
-	    email_id = params[:email_id]
-	    donor_user_id = params[:donor_user_id]
-	    event_code = generate_event_code
-	    event_status = "Active"
-	    Event.create(location: => location,:available_meals => available_meals,:phone_number => pno,:email_id => email_id,:donor_user_id => donor_user_id,:event_code => event_code,:status => event_status)
-	    User.find_all_by_user_type("Receiver").each do |user|
+    email_id = params[:email]
+    event_code = generate_event_code
+    event_status = "Active"
+    Event.create(:location => location,:available_meals => available_meals,:phone_number => pno,:email_id => email_id,:donor_user_id => session[:user_id],:event_code => event_code,:status => event_status)
+    User.where("user_type = 'Receiver'").each do |user|
 			UserMailer.send_email(user.email_id,event_code).deliver!
-	    		send_message(user.phone_number,event_code)
-	    end
+	    send_message(user.phone_number,event_code)
+    end
 	end
 
 	def send_message(to,token)
@@ -50,12 +62,17 @@ class FoodController < ApplicationController
 		# to = '+917204225176' # Your mobile phone number
 		client.messages.create(
 			from: from,
-			to: to,
+			to: "+91"+to,
 			body: "Hey! donor is waiting.Please use #{token} to order food"
 		)
 	end
 	
 	def create_order(params)
-		order = Order.create(:receiver_user_id => params[:receiver_user_id],:donor_user_id => params[:donor_user_id],:no_of_meals => params[:no_of_meals],:event_id => params[:event_id])
+		code = params[:event_code]
+		event = Event.find_by_event_code(code)
+		p "============create_order=========="
+		p code
+		p event
+		order = Order.create(:receiver_user_id => params[:receiver_user_id],:donor_user_id => event.donor_user_id,:no_of_meals => params[:no_of_meals],:event_id => event.id)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'food/login_action'
   get 'food/receiver'
   get 'food/donor'
+  post 'food/create_event'
+  post '/food/place_order'
   root :to => "food#login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
