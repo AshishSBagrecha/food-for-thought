f31407b8213a99f3496ec5d47fc4b4592b22f8b8
Ashish Bagrecha <ashish.bagrecha@pramata.com>
2019-07-05 07:23:54 +0000

    food_controller.rb edited online with Bitbucket

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -23,6 +23,26 @@ class FoodController < ApplicationController
   end
 
 	private
+  	def generate_event_code
+		charset = Array('A'..'Z') + Array('a'..'z')
+		code = Array.new(6) { charset.sample }.join
+		return code
+	end
+
+	def create_event(params)
+		location = params[:location]
+		available_meals = params[:available_meals]
+		pno = params[:phone_number]
+	    email_id = params[:email_id]
+	    donor_user_id = params[:donor_user_id]
+	    event_code = generate_event_code
+	    event_status = params[:status]
+	    Event.create(location: => location,:available_meals => available_meals,:phone_number => pno,:email_id => email_id,:donor_user_id => donor_user_id,:event_code => event_code,:status => event_status)
+	    User.find_all_by_user_type("Receiver").each do |user|
+	    		send_message(user.phone_number,event_code)
+	    end
+	end
+
 	def send_message(to,token)
 		account_sid = 'AC94a35860e3eff571097ba9b0b8723bd0'
 		auth_token = '8200d6c7147f78dd987e35d4accf20ff'
@@ -32,7 +52,7 @@ class FoodController < ApplicationController
 		client.messages.create(
 			from: from,
 			to: to,
-			body: "Hey! Please use #{token} to order food"
+			body: "Hey! donor is waiting.Please use #{token} to order food"
 		)
 	end
 end
